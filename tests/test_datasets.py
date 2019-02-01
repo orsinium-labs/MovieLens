b8894d202e41c6b1f4c8034b9e36e0e73cc23934
gram <master_fess@mail.ru>
2019-02-01 11:22:00 +0300

    +tests for dataset

---
+++ tests/test_datasets.py
@@ -0,0 +1,18 @@
+from movielens.datasets import RatingData
+import numpy
+
+
+def test_users():
+    ds = RatingData(full=False)
+    users = ds.users
+    assert 1 in users
+    assert 2 in users
+    assert numpy.where(users == 1) < numpy.where(users == 2)
+
+
+def test_movies():
+    ds = RatingData(full=False)
+    movies = ds.movies
+    assert 1 in movies
+    assert 2 in movies
+    assert numpy.where(movies == 1) < numpy.where(movies == 2)
